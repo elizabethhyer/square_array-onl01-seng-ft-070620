5d0ac133d7db6db449570f838ebe81bfe622f525
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 22:26:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  array.each do |x|
+ << "|x| ** |x|"
+  end
+end
+
+
+
+def square_array(array)
+  # your code here
+ results = []
+  array.each do |x|
+  
 end
